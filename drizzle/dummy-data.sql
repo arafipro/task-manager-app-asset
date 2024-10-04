e645c847e5303fec6825a35b4a47bd30c509ac1e
Hiroki Chiyoda <contact@arafipro.com>
2024-10-04 15:22:06 +0900

    add drizzle/dummy-data.sql

---
+++ drizzle/dummy-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO task_table (id, title, description, due_date, completed, important, user_id) VALUES
+(1, 'プロジェクト提案書の完成', 'ドラフトを完成させ、レビューのために送信', '2024-09-30', 0, 1, NULL),
+(2, 'チームミーティング', 'プロジェクトのタイムラインとマイルストーンについて話し合う', '2024-09-25', 1, 0, NULL),
+(3, 'React Hooksを学ぶ', 'useEffectとuseContextを勉強する', '2024-09-05', 0, 0, NULL);
